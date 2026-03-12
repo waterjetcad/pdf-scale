30955d7ad88d02a70836ab302a28d16656307ce7
cody <Ranneycody11@gmail.com>
2026-03-12 15:29:04 -0400

    editss

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    return config;
-  },
+  serverExternalPackages: ["canvas"],
 };
 
 export default nextConfig;
